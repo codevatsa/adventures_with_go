ccfc95199155081201e63b603cdf291732cd5abc
Pranav Srivatsa <pranavsrivatsa@protonmail.com>
2022-01-07 23:04:09 +0530

    feat: hello-world: First commit for go program- The hello world program

---
+++ hello_world/hello_world.go
@@ -0,0 +1,9 @@
+// The Hello world program is usually written to appease the programming gods before learning a new language
+// go run hello_world.go to run the program
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
